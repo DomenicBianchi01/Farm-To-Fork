3e2a3462d0ff58887289c3a072affdc556c40ae7
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-02-13 19:49:23 -0500

    Adding the base of the project

---
+++ Farm To Fork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Farm To Fork
+//
+//  Created by Domenic Bianchi on 2018-02-13.
+//  Copyright © 2018 Domenic Bianchi & Marshall Asch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
