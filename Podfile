c89ccc6dd7c01861aece20f059ac2778ecbca240
Domenic Bianchi <dbianchi@uoguelph.ca>
2018-03-08 14:02:48 -0500

    Actually adding pods

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'Farm To Fork' do
+  use_frameworks!
+  pod 'TextFieldEffects'
+end
---
+++ Pods/Target Support Files/Pods-Farm To Fork/Pods-Farm To Fork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Farm_To_Fork : NSObject
+@end
+@implementation PodsDummy_Pods_Farm_To_Fork
+@end
